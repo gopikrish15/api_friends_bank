fa547d9250efe1fdfb948acc078622622e0166fb
mgopi <gopikrishnan972000@gmail.com>
2023-09-21 11:11:51 +0530

    delete api works

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,11 +50,8 @@ exports.updateMembersWithID = async (req, res) => {
 exports.deleteMembersWithID = async (req, res) => {
     try {
         const { id } = req.params
-        const member = await Member.findByIdAndDelete(id, req.body)
-        if (!member) {
-            return res.status(404).json({ message: `cannot find any member with ID ${id}` })
-        }
-        res.status(200).json(updateProduct);
+        const member = await Member.findByIdAndRemove(id, req.body)
+        res.status(200).json(member);
 
     } catch (error) {
         res.status(500).json({ message: error.message })
